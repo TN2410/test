ac43acba324eaa01f4c2443b3103191f8e1d5a29
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-03-28 16:52:44 +0900

    1st commit

--- streamlit.py
+++ streamlit.py
@@ -8,7 +8,7 @@ sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\Scrip
 
 import streamlit as st
 
-df=pd.DataFrame(np.random.rand(100,2)/[20,20]+[35.19,138.80],columns=["lat","lon"])
+df=pd.DataFrame(np.random.rand(100,2)/[20,20]+[35.19,138.85],columns=["lat","lon"])
 
 st.text("test")
 st.slider("testrrrst",0,100,50)
