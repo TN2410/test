bfb7577267ebeabc91c323f091866c84e3bec195
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 15:52:42 +0900

    1st commit

--- streamlit.py
+++ streamlit.py
@@ -10,23 +10,23 @@ import streamlit as st
 
 dirname = r'C:\Users\1219829\OneDrive - トヨタ自動車株式会社\○開発資料\WEC\FFT\DYNO_TEST\P0053\202503_PdSFTFr差\317_Dyno_File_0005_404_2025_HPP RH Z_accum_400.csv'
 
-df=pd.read_csv(dirname, encoding='utf-8',skiprows=0)#ベースデータの読み込み
+#df=pd.read_csv(dirname, encoding='utf-8',skiprows=0)#ベースデータの読み込み
 print(len(df.columns))
 
 #t =0.8 * np.array(range(len(df.columns)-1))
 #最上行のカラム名を数値化し　neと定義する
-ne=df.columns[1:].astype(float)
+#ne=df.columns[1:].astype(float)
 #回転数×時間のグラフ
 #plt.plot(t,ne)
 #plt.show()
 
-st.slider(0,int(y))
+st.slider(0,500)
 
 #全時間窓の代表回転数と周波数分析結果
-for j in range(1,len(df.columns)):
+#for j in range(1,len(df.columns)):
     
-    x=df.iloc[:,0]
-    y=df.iloc[:,j]
+#    x=df.iloc[:,0]
+#    y=df.iloc[:,j]
     
 #    plt.plot(x,y)
 #    plt.show()
