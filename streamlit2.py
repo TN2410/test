45c3f10f8eb13c24bdf7a9f119980ba286d41d2b
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 17:43:49 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -12,4 +12,4 @@ if os.path.exists(dirname):
     age = st.slider("ファイルを選択してください",0,40,20)
 else:
 	print("File does not exist!")
-	age = st.slider("ファイルを選択してください",0,100,20)
+	age = st.slider("ファイルを選択してください",0,80,20)
