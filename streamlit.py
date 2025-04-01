d232f7a20079b3758e48c4b8f5326e72608ae8fe
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 10:53:16 +0900

    first commit

--- streamlit.py
+++ streamlit.py
@@ -1,16 +1,50 @@
+#FFT 累積データをstreamlitで表示する
 import sys
-import numpy as np
-import pandas as pd
-
-sys.path.append(r'c:\users\1219829\python\py38\lib\site-packages')
-sys.path.append(r'C:\Users\1219829\python\Py38\Scripts')
+sys.path.append(r'c:\users\1219829\python\py312\lib\site-packages')
+sys.path.append(r'C:\Users\1219829\python\Py312\Scripts')
 sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\Scripts')
-
+                
+import os
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+import IPython.display
+from IPython.display import display
+import pandas as pd
 import streamlit as st
 
-df=pd.DataFrame(np.random.rand(100,2)/[20,20]+[35.19,138.90],columns=["lat","lon"])
+dirname = r'C:\Users\1219829\Desktop\dpu\data\317_Dyno_File_0005_404_2025_HPP RH Z_accum_400.csv'
 
-st.text("test")
-st.slider("testrrrst",0,100,50)
-st.map(df)
-st.slider("testtest",0,10,5)
+df=pd.read_csv(dirname, encoding='utf-8',skiprows=0)#ベースデータの読み込み
+print(len(df.columns))
+
+t =0.8 * np.array(range(len(df.columns)-1))
+#最上行のカラム名を数値化し　neと定義する
+ne=df.columns[1:].astype(float)
+#回転数×時間のグラフ
+plt.plot(t,ne)
+plt.show()
+
+st.slider(0,int(t))
+
+#全時間窓の代表回転数と周波数分析結果
+for j in range(1,len(df.columns)):
+    
+    x=df.iloc[:,0]
+    y=df.iloc[:,j]
+    
+    plt.plot(x,y)
+    plt.show()
+
+# st.session_state
+# st.write
+# st.json
+# st.data_editer
+# st.columns
+# st.empty
+# st.container
+# st.button(callback)
+# st.spinner
+# st.expander
+# st.slidebar
+# st.swith
