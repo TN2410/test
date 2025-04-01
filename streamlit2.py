4b102c91449fe3d931d07af108334fd0c202630e
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 17:25:15 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -1,12 +1,15 @@
 #FFT 累積データをstreamlitで表示する
 
-
+import os
 import streamlit as st
 import pandas as pd
 import os
 
 dirname = r'C:\Users\1219829\desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
 
-df=pd.read_csv(dirname,encoding="utf-8",skiprows=0)
+if os.path.exists(dirname):
+    df = pd.read_csv(dirname, encoding="utf-8", skiprows=0)
+else:
+    print("File does not exist!")
 
 age = st.slider("ファイルを選択してください", 0, 100, 20)
