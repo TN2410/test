6dc9f5f739b649e29a57d3d5848b6e42cdf45d4c
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 17:34:07 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -10,7 +10,10 @@ dirname = r'C:\Users\1219829\desktop\python\streamlit\317_Dyno_File_0003_404_520
 if os.path.exists(dirname):
 	df = pd.read_csv(dirname, encoding="utf-8", skiprows=0)
 	print(df)
+	age = st.slider("ファイルを選択してください", 0, 40, 20)
+	
+
 else:
 	print("File does not exist!")
 
-age = st.slider("ファイルを選択してください", 0, 40, 20)
+	age = st.slider("ファイルを選択してください", 0, 100, 20)
