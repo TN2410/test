9c72508fa86b36fd141c43b69a364828c45bb977
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 17:10:45 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -1,22 +1,13 @@
 #FFT 累積データをstreamlitで表示する
 
+
 import streamlit as st
 import pandas as pd
 import os
-import numpy as np
 
+dirname = r'C:\Users\1219829\desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
+
+#df=pd.read_csv(dirname,encoding="CP932",skiprows=0)
 
 age = st.slider("ファイルを選択してください", 0, 100, 20)
 
-st.title("データフレームの表示")
-
-df2 = pd.read_csv(r'C:\Users\1219829\OneDrive - トヨタ自動車株式会社\○開発資料\WEC\FFT\DYNO_TEST\P0053\202503_PdSFTFr差\317_Dyno_File_0005_404_2025_HPP RH Z_accum_400.csv')
-
-df = pd.DataFrame(
-    np.random.rand(20, 3),
-    columns=['Column A', 'Column B', 'Column C']
-)
-
-st.write("▼DataFrameの表示例")
-st.dataframe(df)  # データフレーム表示
-st.dataframe(df2)  # データフレーム表示
