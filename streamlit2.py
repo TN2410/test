cad19fef09941910619a9bea87c77f60377682e0
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 17:45:08 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -7,9 +7,9 @@ import pandas as pd
 dirname = r'C:\Users\1219829\desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
 
 if os.path.exists(dirname):
-    df = pd.read_csv(dirname, encoding="utf-8", skiprows=0)
-    print(df)
-    age = st.slider("ファイルを選択してください",0,40,20)
+	df = pd.read_csv(dirname, encoding="utf-8", skiprows=0)
+	print(df)
+	age = st.slider("ファイルを選択してください",0,40,20)
 else:
 	print("File does not exist!")
 	age = st.slider("ファイルを選択してください",0,80,20)
