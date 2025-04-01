cb0cee13d93fdb380a70de9a36349496ef3ad6f7
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 17:49:01 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -4,7 +4,7 @@ import os
 import streamlit as st
 import pandas as pd
 
-dirname = 'C:\Users\1219829\desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
+dirname = r'C:\Users\1219829\Desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
 
 if os.path.exists(dirname):
 	df = pd.read_csv(dirname, encoding="utf-8", skiprows=0)
