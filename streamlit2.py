d3a94eac72047811688547fd292a42c4b1dac2bf
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 16:37:10 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -7,6 +7,8 @@ import os
 
 dirname = r'C:\Users\1219829\OneDrive - トヨタ自動車株式会社\○開発資料\WEC\FFT\DYNO_TEST\P0053\202503_PdSFTFr差\317_Dyno_File_0005_404_2025_HPP RH Z_accum_400.csv'
 
+df=pd.read_csv(dirname,encoding="CP932",skiprows=0)
+
 
 age = st.slider("ファイルを選択してください", 0, 100, 20)
 
