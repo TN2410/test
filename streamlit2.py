2a7132244dd2586ed82a64a8c8d1a8018fccebc2
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 17:23:11 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -7,6 +7,6 @@ import os
 
 dirname = r'C:\Users\1219829\desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
 
-#df=pd.read_csv(dirname,encoding="utf-8",skiprows=0)
+df=pd.read_csv(dirname,encoding="utf-8",skiprows=0)
 
 age = st.slider("ファイルを選択してください", 0, 100, 20)
