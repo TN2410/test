ae9b200ed77497114e6681a0918f6f858d5fd9a5
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 16:21:41 +0900

    1st commit

---
+++ streamlit2.py
@@ -0,0 +1,11 @@
+#FFT 累積データをstreamlitで表示する
+import sys
+sys.path.append(r'c:\users\1219829\python\py312\lib\site-packages')
+sys.path.append(r'C:\Users\1219829\python\Py312\Scripts')
+sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\Scripts')
+                
+
+import streamlit as st
+
+st.slider(0,500,500)
+
