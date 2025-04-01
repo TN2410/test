90eebbcb48a7100090eb447e313498a32c22520b
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-01 17:01:16 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -10,6 +10,8 @@ age = st.slider("ファイルを選択してください", 0, 100, 20)
 
 st.title("データフレームの表示")
 
+df2 = pd.read_csv(r'C:\Users\1219829\OneDrive - トヨタ自動車株式会社\○開発資料\WEC\FFT\DYNO_TEST\P0053\202503_PdSFTFr差\317_Dyno_File_0005_404_2025_HPP RH Z_accum_400.csv')
+
 df = pd.DataFrame(
     np.random.rand(20, 3),
     columns=['Column A', 'Column B', 'Column C']
@@ -17,3 +19,4 @@ df = pd.DataFrame(
 
 st.write("▼DataFrameの表示例")
 st.dataframe(df)  # データフレーム表示
+st.dataframe(df2)  # データフレーム表示
