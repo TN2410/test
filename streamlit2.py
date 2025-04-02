05df9e415fccfa844423be9ed294be5bac487b98
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-02 17:00:37 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -6,13 +6,11 @@ import pandas as pd
 
 filename = r'C:\Users\1219829\Desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
 
-df = pd.read_csv(filename, encoding="utf-8", skiprows=0)
-
-st.dataframe(df.columns)
+#df = pd.read_csv(filename, encoding="utf-8", skiprows=0)
+#st.dataframe(df.columns)
 	
-st.title(df.columns)
+st.title("filename")
 age = st.slider("ファイルを選択してください",0,40,20)
-
 
 age = st.slider("ファイルを選択してください",0,800,20)
 age = st.slider("ファイルを選択してください",0,100,20)
