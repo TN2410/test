dd2dc528ac803270d2e8288353cc1ab9c94260a4
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-02 16:55:22 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -4,14 +4,14 @@ import os
 import streamlit as st
 import pandas as pd
 
-dirname = r'C:\Users\1219829\Desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
+filename = r'C:\Users\1219829\Desktop\python\streamlit\317_Dyno_File_0003_404_520wk_HPP LH X_accum_400.csv'
 
-if os.path.exists(dirname):
-	df = pd.read_csv(dirname, encoding="utf-8", skiprows=0)
+if os.path.exists(filename):
+	df = pd.read_csv(filename, encoding="utf-8", skiprows=0)
 	print(df.columns)
 	st.title(df.columns)
 	age = st.slider("ファイルを選択してください",0,40,20)
 else:
-	st.title(dirname)
+	st.title(filename)
 	age = st.slider("ファイルを選択してください",0,800,20)
 	age = st.slider("ファイルを選択してください",0,100,20)
