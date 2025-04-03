d4e5a025efe40537ad7204b934e7607c343dc03a
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-03 11:48:03 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -4,6 +4,6 @@ import numpy as np
 
 data = pd.DataFrame({
     'weight': np.random.normal(150, 10, 1000),
-    'height': np.random.normal(100, 20,5000)
+    'height': np.random.normal(100, 20,800)
 })
 st.scatter_chart(data, x="weight", y="height")
