44118398722ada53cba9fb75a94729bb2afacb31
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-03 18:37:21 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -12,11 +12,25 @@ f = st.file_uploader("CSVファイルをアップロードしてください", t
 # ファイルがアップロードされた場合
 if f is not None:
     df = pd.read_csv(f,index_col=0)
-    filename= os.path.basename(f)
-    filename_no_extension = os.path.splitext(filename)[0]
     st.write("アップロードされたデータフレーム:")
     st.write(df)
     st.title(filename_no_extension)
+
+    # matplotlibで図を用意する
+    fig = plt.figure()
+    time=np.arrange(0,len(df.columns),1)
+    plt.plot(time, df.columns, marker='.', markersize=10)
+    #plt.xlim(0.5, 12.5)
+    #plt.ylim(0, 40)
+    plt.title('Average Temperature at Kyoto in 2018', fontsize=15)
+    plt.xlabel('month', fontsize=10)
+    plt.ylabel('average temperature (deg)', fontsize=10)
+
+    # streamlit plot
+    st.pyplot(fig)
+
+
+
 
     age = st.slider("ファイルを選択してください",0,800,20)
     age = st.slider("ファイルを選択してください",0,100,20)
