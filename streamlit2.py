502b07dbd9b55e8fc787e6d82e401e7f5bd078e9
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-04 10:41:31 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -1,16 +1,3 @@
-import sys
-
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\python312.zip')
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\DLLs')
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\Lib')
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312')
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\Lib\site-packages')
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\Lib\site-packages\win32')
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\python312.zip')
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\Lib\site-packages\win32\lib')
-sys.path.append(r'C:\Users\1219829\AppData\Local\Programs\Python\Python312\Lib\site-packages')
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -25,18 +12,17 @@ f = st.file_uploader("CSVファイルをアップロードしてください", t
 # ファイルがアップロードされた場合
 if f is not None:
     df = pd.read_csv(f,index_col=0)
-    df.loc['ONE'] = 0
+    
+    st.title(df.iloc[1,1] )
     st.write("アップロードされたデータフレーム:")
     st.write(df)
 
     time=np.arange(0,8192,1)
 
-    st.scatter_chart(df,x="freq",y="max")
-	
+    st.scatter_chart(df,x="freq",y="max")	
 
     #plt.xlim(0.5, 12.5)
     #plt.ylim(0, 40)
-
 
     age = st.slider("ファイルを選択してください",0,800,20)
     age = st.slider("ファイルを選択してください",0,100,20)
