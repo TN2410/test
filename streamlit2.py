cd2a529d5215be738e51527dfe3c11e95c3f71b5
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-04 12:35:46 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -5,7 +5,6 @@ import os
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-
 #FFT 累積データをstreamlitで表示する
 # ファイルアップロード
 
@@ -15,17 +14,24 @@ f = st.file_uploader("CSVファイルをアップロードしてください", t
 if f is not None:
     df = pd.read_csv(f,index_col=0)
     st.write("アップロードされたデータフレーム:")
-    x=np.arange(0,100,1)
-    y=np.arange(0,200,2)
+    
+    df = pd.DataFrame(np.arange(100).reshape(10, 10),
+                  columns=['col_0', 'col_1', 'col_2', 'col_3'])
 
-    max_value=x.max()
+    max_value=df["col0"].max()
 
-    min_value=y.min()
+    min_value=df["col0"].min()
     slider=st.slider("指定範囲", min_value, max_value, max_value, 1)
 
 #上記スライダにて以下データをフィルタリング
 
-    x = [x <=slider]
+    df = df(df["col_0"] <=slider])
+
+st.write(
+    px.bar(df, x=df['col_0'], y=df['col_1'] ,title="sample figure",color='nation')
+)
+
+
 
     plt.plot(x, y)
     plt.title('Matplotlib and Streamlit')
