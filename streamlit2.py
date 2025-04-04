4095e35eb6381653c7182b6f213d7d14ff463731
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-04 12:59:35 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -16,16 +16,17 @@ if f is not None:
     
     df = pd.DataFrame(data={'col_0': np.arange(0,100,1), 'col_1': np.arange(0,200,2)})
 
-    max_value=df["col0"].max()
+    max_value=df['col0'].max()
 
-    min_value=df["col0"].min()
+    min_value=df['col0'].min()
     slider=st.slider("指定範囲", min_value, max_value, max_value, 1)
 
 #上記スライダにて以下データをフィルタリング
 
-    df = df[df["col_0"] <=slider]
+    df = df[df['col_0'] <=slider]
 st.write(
-    px.bar(df, x=df['col_0'], y=df['col_1'] ,title="sample figure",color='nation')
+    px.bar(df, x=df['col_0'], y=df['col_1'] ,title='sample figure')
 )
 
 
+'
