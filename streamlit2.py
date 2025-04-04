432fa4904e9683842ee41dc9cadd1dd2563bbbdf
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-04 15:08:28 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -16,11 +16,13 @@ if f is not None:
     max_value=int(df["freq"].max())
     min_value=int(df["freq"].min())
     slider=st.slider("下限範囲", min_value, max_value, max_value, 10)
-    slider2=st.slider("上限範囲", min_value, max_value, max_value, 10)
 
 #上記スライダにて以下データをフィルタリング
 
     df = df[df["freq"] >=slider]
+    
+    slider2=st.slider("上限範囲", min_value, max_value, max_value, 10)
+
     df2 = df[df["freq"] <=slider2]
 
     st.scatter_chart(df2,x=df2["fre],y=df2["max"])
