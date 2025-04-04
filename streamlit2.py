385d64dd95acf660df40d17326a4afe62f65ab28
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-04 15:47:50 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -11,7 +11,7 @@ f = st.file_uploader("CSVファイルをアップロードしてください", t
 
 # ファイルがアップロードされた場合
 if f is not None:
-    df = pd.read_csv(f)
+    df = pd.read_csv(f,index=0)
     st.write("アップロードされたデータフレーム:")
     
     st.write(df)
