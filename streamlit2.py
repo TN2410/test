57ca18385896130d2a61b9d5a2342475c1e8a518
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-10 17:38:32 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -1,31 +1,42 @@
+#FFT 累積データをstreamlitで表示する
+# ファイルアップロード
+
+%matplotlib inline
+import os,time,gc,math,glob,re,win32gui,ctypes,win32con
 import streamlit as st
 import pandas as pd
 import numpy as np
 import os
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 #FFT 累積データをstreamlitで表示する
 # ファイルアップロード
 
 f = st.file_uploader("CSVファイルをアップロードしてください", type="csv")
-
+#f=r"C:\Users\1219829\Desktop\python\streamlit\ff.csv"
+rpm=[]
 # ファイルがアップロードされた場合
 if f is not None:
     df = pd.read_csv(f,index_col=0)
+
+    df.loc['time'] = np.arange(0,len(df.columns))
+
+    print(df)
+    
+    figure=plt.figure()
+    y = df.columns.tolist()
+    x = df.loc['time'] 
+    plt.plot(x,y)
+    plt.show()
+    
+    plt.savefig(r'C:\Users\1219829\Desktop\python\streamlit\figure01.jpg')
+ 
+    st.scatter_chart(df,y=df.columns.tolist())
     st.write("アップロードされたデータフレーム:")
-    
-    st.write(df)
-    max_value=int(df["freq"].max())
-    min_value=int(df["freq"].min())
-
-    slider=st.slider("下限範囲", min_value, max_value, min_value, 1)
-    df = df[df["freq"] >= slider]
-    
-    st.write(df)
-
-    st.scatter_chart(df,x=df['freq'],y=df['max'])
-
+    #slider=st.slider("下限範囲", min_value, max_value, max_value, 1)
+    #df = df[df["freq"] <= slider]
+    #st.write(df)
 #    slider2=st.slider("上限範囲", min_value, max_value, max_value, 1)
 #    df2 = df[df["freq"] <= slider2]
-
 #    st.scatter_chart(df2,x=df2["freq"],y=df2["max"])
