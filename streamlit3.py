7deea22cf9f7823545304ea5fccccfc6cf03379e
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-11 13:11:23 +0900

    1st commit

--- streamlit3.py
+++ streamlit3.py
@@ -11,12 +11,12 @@ import plotly.express as px
 #FFT 累積データをstreamlitで表示する
 # ファイルアップロード
 
-f = st.file_uploader("CSVファイルをアップロードしてください", type="csv")
+f = st.file_uploader("txtファイルをアップロードしてください", type="txt")
 #f=r"C:\Users\1219829\Desktop\python\streamlit\ff.csv"
 rpm=[]
 # ファイルがアップロードされた場合
 if f is not None:
     df = pd.read_csv(f,sep="[\t\0]",index_col=0)
-    
 #    slider=st.slider("範囲", min_value, max_value, max_value, 10)
     st.scatter_chart(df,x='Time',y='NE')
+    print(slider)
