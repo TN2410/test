32b5329f9ce81be8de86748e82a5217340136685
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-11 13:51:22 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -7,11 +7,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 #FFT 累積データをstreamlitで表示する
 # ファイルアップロード
 
-f = st.file_uploader("CSVファイルをアップロードしてください", type="csv")
+f = st.file_uploader("accumCSVファイルをアップロードしてください", type="csv")
 #f=r"C:\Users\1219829\Desktop\python\streamlit\ff.csv"
 rpm=[]
 # ファイルがアップロードされた場合
@@ -24,8 +25,11 @@ if f is not None:
     df['ne'] = df['ne'].astype(float)
     print(df['ne'])
 
-    max_value=df['ne'].max()
-    min_value=df['ne'].min()
+    max_value=df['Time'].max()
+    min_value=df['Time'].min()
+
+    df= df[df['Time'] <=slider]
+
     
     slider=st.slider("範囲", min_value, max_value, max_value, 10)
     st.scatter_chart(df,x='time',y='ne')
