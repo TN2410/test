f0545d87c7ec5e20e535ea37fb63fd0f4a5ab29d
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-15 11:08:12 +0900

    1st commit

--- streamlit2.py
+++ streamlit2.py
@@ -32,7 +32,8 @@ if f is not None:
 
     fig = plt.figure()
     plt.plot(df[["Time"]],df[["NE"]])
-
+    st.pyplot(fig)
+    
     plt.plot(df.columns[:-2].tolist(),df.iloc[slider,:-2].tolist())
     plt.xlim(0, 4096)
     
