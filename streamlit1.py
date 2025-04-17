4771f903a17aaad2b191f10439cf1a172c33f166
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-04-17 13:16:50 +0900

    1st commit

---
+++ streamlit1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# 緯度経度データ（10進数）
+pref_list = [
+  {"longitude":135.741667, "latitude":30.789828}, # 熊本県
+  {"longitude":131.423855, "latitude":31.911090}, # 宮崎県
+  {"longitude":130.557981, "latitude":31.560148}, # 鹿児島県
+]
+
+st.map(pref_list)
