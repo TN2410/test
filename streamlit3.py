4f6e24e17b67e033b19706a3e6a3097fe3b47668
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-05-22 16:49:03 +0900

    1st commit

--- streamlit3.py
+++ streamlit3.py
@@ -18,8 +18,8 @@ def process_b():
 
 
 for j in range(3):
-    check= st.checkbox()
-    
+    check[j]= st.checkbox(j,vakye=True)
+
 
 sample_f = st.file_uploader("txtファイルをアップロードしてください", type="csv")
 if sample_f is not None:
