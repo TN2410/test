af84fffdf8ccd61529b9b428ca366338a501708c
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-05-22 17:22:14 +0900

    1st commit

--- streamlit3.py
+++ streamlit3.py
@@ -28,13 +28,11 @@ if sample_f is not None:
 uploaded_files = st.file_uploader("txtファイルをアップロードしてください", type="txt",accept_multiple_files=True)
 if uploaded_files is not None:
     dataframes = {}#この初期化した辞書型へ読み込んで全ロードデータを保存しておく
-
     for uploaded_file in uploaded_files:
         df = pd.read_csv(uploaded_file,sep="[\t\0]",index_col=0)
         dataframes[uploaded_file.name] = df
-    #保存完了
-
-# 散布図のプロット
+    #　保存完了
+    #　散布図のプロット
     if dataframes:
         fig=plt.figure(figsize=(10, 6))
         # 各データフレームの表示を制御するボタンを作成
@@ -45,46 +43,27 @@ if uploaded_files is not None:
             # ボタンが選択されている場合に散布図をプロット
             if show_data:
                 # x列とy列を指定（ここでは仮に 'x' と 'y' 列を使用）
-    
                 selected_xdata = df[x_pal]
                 selected_ydata = df[y_pal]
-                df["Time0"]=np.arange(len(df)).astype(float)
+                ＃＃df["Time0"]=np.arange(len(df)).astype(float)
                 #st.line_chart(selected_data)
-            
                 x=selected_xdata[1:].astype(float)
                 y=selected_ydata[1:].astype(float)
-
                 plt.scatter(x, y,label=filename)
                 #plt.title(file.name)
         plt.legend(bbox_to_anchor=(1.05, 1.0), loc="upper left")
         plt.xlabel(x_pal)
         plt.ylabel(y_pal)
         st.pyplot(fig)
-
     #     plt.ylim(slider2, slider)
     #     plt.xlim(slider4, slider3)
-
-    # #smpファイルよりチェックRAM名を読み取り
-    # #結果表示
     #     df["NE"][1:] = df["NE"][1:].astype(int)
     #     max_value = df["NE"][1:].max()
     #     min_value = df["NE"][1:].min()
-
-    #     df["Time"] = np.arange(len(df))
-
     #     slider=st.slider("範囲", min_value, max_value, max_value, 10)
     #     slider2=st.slider("範囲", min_value, max_value, 0, 10)
     #     slider3=st.slider("範囲", min_value, max_value, max_value, 100)
     #     slider4=st.slider("範囲", min_value, max_value, 0, 100)
-
     #    df_ne=df[df["NE"][1:]<slider]
-    #　　データフレーム操作必要
-    #
-    #   st.scatter_chart(df,x="Time",y="NE")
-    #グラフの表示変更はOK
 
-
-        # if st.checkbox(testtest):
-        #     st.write('グラフを表示する')
-        #     st.pyplot(fig)
 
