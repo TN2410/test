9a138424f387e8a820a8525ed56c4d0f4ed29d7c
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-05-22 16:26:12 +0900

    1st commit

--- streamlit3.py
+++ streamlit3.py
@@ -26,7 +26,7 @@ if sample_f is not None:
     y_pal=st.multiselect('y列を選択してください', newlist)
 
 uploaded_files = st.file_uploader("txtファイルをアップロードしてください", type="txt",accept_multiple_files=True)
-if f is not None:
+if uploaded_files is not None:
     dataframes = {}
     for uploaded_file in uploaded_files:
         df = pd.read_csv(uploaded_file,sep="[\t\0]",index_col=0)
