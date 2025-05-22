285e7fba21cd42de009ce0a45ea1506e5196475b
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-05-22 09:23:28 +0900

    1st commit

--- streamlit3.py
+++ streamlit3.py
@@ -18,7 +18,7 @@ def process_b():
 
 sample_f = st.file_uploader("txtファイルをアップロードしてください", type="csv")
 if sample_f is not None:
-    sample_df = pd.read_csv(sample_f)
+    sample_df = pd.read_csv(sample_f,encoding="CP932")
     sample_par = sanmpe_df.iloc[:,5]
 
     for i in sample_par:
