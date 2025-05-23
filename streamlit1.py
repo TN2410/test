0e0ab5b58da494de2b3260cf61f464cb1e1baeb9
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-05-23 12:00:08 +0900

    1st commit

--- streamlit1.py
+++ streamlit1.py
@@ -9,7 +9,6 @@ pref_list = [
   {"longitude":131.423855, "latitude":31.911090}, # 宮崎県
   {"longitude":130.557981, "latitude":31.560148}, # 鹿児島県
 ]
-
 st.map(pref_list)
 st.number_input("input_your_number")  
 # チェックボックス
@@ -17,8 +16,6 @@ st.checkbox("CHECK")
 if st.button("bot", key='my_button', help='このボタンをクリックしてアクションを実行します'):
    st.write('ボタンがクリックされました！')
 
-
 #入力値でカラム名を指定し、グラフを書く。dpuとのメリット複数のファイルを表示可能か？。
 year=st.number_input('年(1952~5年おき)',1952,2007,1952,step=5)
 df_px=df[df['year'] ==year]
-
