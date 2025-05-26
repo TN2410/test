5b5bcd59c55920b17a589a1a9ecda6d511302581
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-05-26 11:31:27 +0900

    1st commit

--- easytest.py
+++ easytest.py
@@ -1,16 +1,16 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.write("test")
 
 x = list(range(50))
 y = np.random.randn(50)
 
-# plt.plot(x, y)
-# plt.title('Matplotlib and Streamlit')
-# plt.xlabel('X-axis')
-# plt.ylabel('Y-axis')
+plt.plot(x, y)
+plt.title('Matplotlib and Streamlit')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
 
 # Streamlitで表示
-# st.pyplot(plt)
+st.pyplot(plt)
