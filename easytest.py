9bb519edbbda1a137faaa22c900e1a5f4541a8a3
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-05-28 16:13:33 +0900

    1st commit

--- easytest.py
+++ easytest.py
@@ -1,11 +1,10 @@
-import sys
-sys.path.append(r'c:\users\1219829\python\py38\lib\site-packages')
-sys.path.append(r'C:\Users\1219829\python\Py38\Scripts')
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
 st.write("test")
+
+fig=plt.figure()
 
 x = list(range(50))
 y = np.random.randn(50)
@@ -16,4 +15,4 @@ plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 
 # Streamlitで表示
-st.pyplot(plt)
+st.pyplot(fig)
