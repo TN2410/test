029e3bce7cf40bad08d1179bb608ce2e29b72f1a
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-03 20:07:57 +0900

    1st commit

--- fft_accum.py
+++ fft_accum.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 st.title("振動とPmax周波数の相関")
 
-with st.expander("アップロード")
+with st.expander("アップロード"):
     uploaded_files = st.file_uploader("accum.CSVファイルをアップロード(複数可)", type="csv",accept_multiple_files=True)
 #st.write("check22")
 #df_ne=df[df["NE"][1:]<slider]
