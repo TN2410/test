598c187b033f4395691569731f114089c17440eb
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-04 11:03:28 +0900

    1st commit

--- fft_accum.py
+++ fft_accum.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import japanize-matplotlib
+import japanize_matplotlib
 
 st.title("振動とPmax周波数の相関")
 
