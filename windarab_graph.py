52ed9cc1e98b40ebde937f36c96c737541645275
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-06 11:59:25 +0900

    1st commit

--- windarab_graph.py
+++ windarab_graph.py
@@ -19,7 +19,7 @@ if uploaded_files is not None:
     for uploaded_file in uploaded_files:
        #ファイルを簡易的に読み込んでwindarabデータを　5行削除する
         st.write(uploaded_file)
-        with open(uploaded_file.name,"r") as file:
+        with open(uploaded_fileS,"r") as file:
             first_line = file.readline().strip()  # 最初の1行を読み込み、前後の空白を削除
             if "BOSCH-DARAB" in first_line: 
                 skiprows = 5
