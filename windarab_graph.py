b99fd2bce5204d2995522e4cdda47cf97a1f1bf7
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-06 17:52:00 +0900

    1st commit

--- windarab_graph.py
+++ windarab_graph.py
@@ -34,7 +34,8 @@ if uploaded_files is not None:
 
 sample_f = st.file_uploader("csvファイルをアップロードしてください", type="csv")
 if sample_f is not None:
-    sample_df = pd.read_csv(sample_f,encoding="CP932")
+    with open(uploaded_file,"r") as file:
+        st.write("test")
     sample_par = sample_df.iloc[:,sample_columns]#DPU用 sample_columns 2 or 5
     mylist = [str(x) for x in sample_par]
     newlist = [x for x in mylist if x != "nan"]
