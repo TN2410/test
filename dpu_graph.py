823dc2ee419aa8da777922a162445138db940e62
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-06 18:17:32 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -30,6 +30,8 @@ uploaded_files = st.file_uploader("txtファイルをアップロードしてく
 if uploaded_files is not None:
     dataframes = {}#この初期化した辞書型へ読み込んで全ロードデータを保存しておく
     for uploaded_file in uploaded_files:
+        with open(uploaded_file.name,"r") as file:
+            st.write("test")
         df = pd.read_csv(uploaded_file,sep="[\t\0]",engine='python')#index_col = 0
         dataframes[uploaded_file.name] = df
         # 時間データを秒に換算する 
