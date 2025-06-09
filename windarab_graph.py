39c691c06259e398b629eca22cb0c0bfe1443aa2
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-09 10:57:14 +0900

    1st commit

--- windarab_graph.py
+++ windarab_graph.py
@@ -23,8 +23,8 @@ if uploaded_files is not None:
     dataframes = {}#この初期化した辞書型へ読み込んで全ロードデータを保存しておく
     for uploaded_file in uploaded_files:
        #ファイルを簡易的に読み込んでwindarabデータを　5行削除する
-        df = pd.read_csv(uploaded_file,sep="\t",encoding ='CP932',low_memory=False,nrows=1)
-        if "BOSCH-DARAB" in df.columns: 
+        df0 = pd.read_csv(uploaded_file,sep="\t",encoding ='CP932',low_memory=False,nrows=1)
+        if "BOSCH-DARAB" in df0.columns: 
             skiprows = 5
             sample_columns = 2
         else:
