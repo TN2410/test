1f56f2627b317a93200eb29ffaa60fa049389449
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-09 11:07:20 +0900

    1st commit

--- windarab_graph.py
+++ windarab_graph.py
@@ -32,13 +32,14 @@ if uploaded_files is not None:
             sample_columns = 5
         df = pd.read_csv(uploaded_file,sep="\t",encoding ='CP932',low_memory=False,skiprows=skiprows)
         dataframes[uploaded_file.name] = df
-
+        st.write(df)
         if "Time" in df.columns:
             time_format = "%H:%M:%S.%f"
             df["Time"][1:] = [datetime.strptime(time_str, time_format) for time_str in df["Time"][1:]]
             init_time = df["Time"][1]
             df["Time"][1:] = [(time - init_time).seconds for time in df["Time"][1:]]
         st.write(df.columns)
+
 sample_f = st.file_uploader("csvファイルをアップロードしてください", type=["csv"])
 if sample_f is not None:
     sample_df = pd.read_csv(sample_f,encoding ='CP932')
