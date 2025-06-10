b7de471e397a0f6a94b5525ada4c6d2e6b3f630c
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-10 16:32:11 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -31,24 +31,25 @@ if uploaded_files is not None:
     dataframes = {}#この初期化した辞書型へ読み込んで全ロードデータを保存しておく
     with st.sidebar:
         lower_bound=st.slider("下限", 0 , 100 , 0 , 1)
-        upper_bound=st.slider("上限", 0 , 100 , 100 , 1)for uploaded_file in uploaded_files:
-        df = pd.read_csv(uploaded_file,sep="[\t\0]",engine='python')
-        df = df.iloc[1:]#dpuの場合は単位行があるために除外する 
-        # 時間データを秒に換算する 
-        time_format = "%H:%M:%S.%f"
-        df["Time"]= [datetime.strptime(time_str, time_format) for time_str in df["Time"]]
-        init_time = df["Time"][1]
-        df["Time"] = [(time - init_time).seconds for time in df["Time"]]
-        df = df.apply(pd.to_numeric)
-        st.write(df[th_pal])
-        max_value = df[th_pal].max()
-        min_value = df[th_pal].min()
-        st.write(min_value,max_value)
+        upper_bound=st.slider("上限", 0 , 100 , 100 , 1)
+        for uploaded_file in uploaded_files:
+            df = pd.read_csv(uploaded_file,sep="[\t\0]",engine='python')
+            df = df.iloc[1:]#dpuの場合は単位行があるために除外する 
+            # 時間データを秒に換算する 
+            time_format = "%H:%M:%S.%f"
+            df["Time"]= [datetime.strptime(time_str, time_format) for time_str in df["Time"]]
+            init_time = df["Time"][1]
+            df["Time"] = [(time - init_time).seconds for time in df["Time"]]
+            df = df.apply(pd.to_numeric)
+            st.write(df[th_pal])
+            max_value = df[th_pal].max()
+            min_value = df[th_pal].min()
+            st.write(min_value,max_value)
 
-            #slider1=st.slider("閾値範囲", min_value, max_value, min_value, 1)
-            #slider2=st.slider("閾値範囲", min_value, max_value, max_value, 1)
-        df = df[(df[th_pal] >= lower_bound) & (df[th_pal] <= upper_bound)]
-        dataframes[uploaded_file.name] = df
+                #slider1=st.slider("閾値範囲", min_value, max_value, min_value, 1)
+                #slider2=st.slider("閾値範囲", min_value, max_value, max_value, 1)
+            df = df[(df[th_pal] >= lower_bound) & (df[th_pal] <= upper_bound)]
+            dataframes[uploaded_file.name] = df
 
     #　散布図のプロット
     if dataframes:
