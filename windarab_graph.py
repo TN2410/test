b0f0f56abffca9f3a4ed0d4c6cc4e5f8523fa700
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-11 16:35:55 +0900

    1st commit

--- windarab_graph.py
+++ windarab_graph.py
@@ -21,7 +21,7 @@ if uploaded_files is not None:
     dataframes = {}#この初期化した辞書型へ読み込んで全ロードデータを保存しておく
     for uploaded_file in uploaded_files:
        #ファイルを簡易的に読み込んでwindarabデータを　5行削除する
-        df0 = pd.read_csv(uploaded_file,sep="\t",encoding="CP932",nrows=6)
+        df0 = pd.read_csv(uploaded_file,sep="\t",encoding="CP932",nrows=1)
         #df = pd.read_csv(uploaded_file,sep="\t",encoding="CP932")
         st.write("df01")
         st.write(df0)
