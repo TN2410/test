dbcf17ca576ea5b9ee68b512c50eccd2de5883b9
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-11 15:11:33 +0900

    1st commit

--- windarab_graph.py
+++ windarab_graph.py
@@ -28,14 +28,11 @@ if uploaded_files is not None:
         if "BOSCH-DARAB" in df0.columns[0]: 
             skiprows = 5
             sample_columns = 2
-            #df = pd.read_csv(uploaded_file, sep="\t",encoding ='CP932',skiprows=skiprows,low_memory=False)#windarabは５ dpuはskiprowsなし
             df = pd.read_csv(uploaded_file, sep="\t",encoding ='CP932',skiprows=skiprows,low_memory=False)#windarabは５ dpuはskiprowsなし
-            st.write(skiprows)
         else:
             skiprows = 0
             sample_columns = 5
             df = pd.read_csv(uploaded_file,sep="\t\0",engine="python",encoding="utf-8")
-            ##df = pd.read_csv(uploaded_file, sep="\t",encoding ='CP932',skiprows=skiprows,low_memory=False)#windarabは５ dpuはskiprowsなし
             dataframes[uploaded_file.name] = df
         st.write(df)##########カラム名が表示されない
         if "Time" in df.columns:
