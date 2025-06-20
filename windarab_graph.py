755310c03c777e078593c89aa8f3c91eababab00
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-06-20 09:51:00 +0900

    1st commit

--- windarab_graph.py
+++ windarab_graph.py
@@ -19,6 +19,7 @@ if uploaded_files is not None:
     for uploaded_file in uploaded_files:
        #ファイルを簡易的に読み込んでwindarabデータを　5行削除する
         content = uploaded_file.read()
+        if "BOSCH-DARAB" in content:
             st.write("test")
         df = pd.read_csv(uploaded_file, sep="\t",encoding ='CP932')#
         #df = pd.read_csv(uploaded_file,encoding = "utf-8" , sep = "\t\0",engine='python')#
