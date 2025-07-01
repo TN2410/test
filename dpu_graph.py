5fe7a822a9968a8f45a0d8adcc56e3160ad84155
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-01 17:14:36 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -35,7 +35,7 @@ if sample_f is not None:
     sample_df = pd.read_csv(sample_f,encoding ='CP932')
     sample = sample_df.iloc[:,sample_columns]#DPU用 sample_columns 2 or 5
     sample_1 = list(filter(None, sample))
-    sample_par = [[s for s in ar if isinstance(s, str)] for ar in sample_1]
+    sample_par = [item for item in sample_1 if item is not None and item != 'nan' and item != math.nan]
     st.write(sample_par)
 
     mylist = [str(x) for x in sample_par]
