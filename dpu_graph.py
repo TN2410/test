775b1150a08598024f871163dec8797a07edddea
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-02 14:30:39 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -116,9 +116,9 @@ if uploaded_files is not None:
                 # plt.bar(x, y)
                 # plt.ylabel("Time(sec)")
                 # plt.title("{}_{:.3f}Hr_{}=<{}<{}".format(y_pal,sumall,lower_bound,th_pal,upper_bound),fontsize="10")
-                x = [1,2,3,4,5])
-                y = [1,2,3,4,5]]
-                z = [1,2,3,4,5]]
+                x = [1,2,3,4,5]
+                y = [1,2,3,4,5]
+                z = [1,2,3,4,5]
                 
                 # figureを生成
                 fig = plt.figure()
