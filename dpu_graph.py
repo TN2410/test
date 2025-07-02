137645cda94ca3f4180f076a851842a1e1adb544
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-02 16:32:39 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -45,18 +45,14 @@ if sample_f is not None:
 
     with st.sidebar:
         x_pal=st.selectbox('x列を選択してください', sample_par) 
+        y_pal=st.selectbox('y列を選択してｋださい', sample_par)
+
         st.write(x_pal,"の")
         #ここでデータを読んで上下限を設定したい
         x_lower_bound = st.number_input('の下限値と',step=1)
-        st.number_input('の下限値と',step=2)
-        st.number_input('の下限値と',step=2)
-        st.number_input('の下限値と',step=2)
-
         x_upper_bound = st.number_input('上限値を入力してください',value=100,step=1) 
         
-        y_pal=st.selectbox('y列を選択してｋださい', sample_par)
-        st.write(y_pal,"の")
-        
+        st.write(y_pal,"の")        
         #ここでデータを読んで上下限を設定したい
         y_lower_bound = st.number_input('の下限値と',step=1)
         y_upper_bound = st.number_input('上限値を入力してください',value=200,step=1) 
