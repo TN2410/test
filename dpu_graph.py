a7e0ffb495f23109d8e43024f92003cb3da0f0a1
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-02 17:16:34 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -84,11 +84,13 @@ if uploaded_files is not None:
 
         for x in range(int(x_lower_bound),int(x_upper_bound),int((x_upper_bound-x_lower_bound)/10)):
             for y in range(int(y_lower_bound),int(y_upper_bound),int((y_upper_bound-y_lower_bound)/10)):
+                st.write(x,y)
                 x_query_string = f"{x_pal} >= {x} & {x_pal} < {x}"    
                 y_query_string = f"{y_pal} >= {y} & {y_pal} < {y}"    
                 
                 x_filtered_data = df.query(x_query_string)
                 y_filtered_data = x_filtered_data.query(y_query_string)
+                st.write(y_filtered_data)
 
                 dataframes[uploaded_file.name,x,y] = len(y_filtered_data)
                 st.write(dataframes)
