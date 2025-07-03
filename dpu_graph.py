a0d7e2176b1c12a52be212b34fc8520325c70070
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-03 09:51:29 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -82,6 +82,7 @@ if uploaded_files is not None:
 
 #分割数　10として　3Dマップを作る
         st.write(uploaded_file.name)
+        map_2d=[]
         for x in range(int(x_lower_bound),int(x_upper_bound),int((x_upper_bound-x_lower_bound)/10)):
             for y in range(int(y_lower_bound),int(y_upper_bound),int((y_upper_bound-y_lower_bound)/10)):
                 x_query_string = f"{x_pal} >= {x} & {x_pal} < {x + int((x_upper_bound-x_lower_bound)/10)}"
@@ -89,8 +90,9 @@ if uploaded_files is not None:
                 x_filtered_data = df.query(x_query_string)
                 y_filtered_data = x_filtered_data.query(y_query_string)
                 ####以下に記載できている？
-                dataframes[uploaded_file.name][(x,y)] = len(y_filtered_data)
-                st.write(dataframes)
+                map_2d[x,y] = len(y_filtered_data)
+
+        dataframes[uploaded_file.name] = map_2d
 
 #各条件での累積時間マップを作成
     #st.write(dataframes)
