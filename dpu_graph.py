f267dbc052a4d2d4f0d95306403ae090f6fe60bc
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-03 09:44:24 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -88,7 +88,7 @@ if uploaded_files is not None:
                 x_filtered_data = df.query(x_query_string)
                 y_filtered_data = x_filtered_data.query(y_query_string)
                 ####以下に記載できている？
-                dataframes{uploaded_file.name,(x,y)} = len(y_filtered_data)
+                dataframes[uploaded_file.name][(x,y)] = len(y_filtered_data)
                 st.write(dataframes)
 
 #各条件での累積時間マップを作成
