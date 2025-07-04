f23e8caea18f6831844468728fafa2801b3abeea
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-04 12:19:43 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -144,7 +144,7 @@ if dataframes:
     sumall = sum(z_values)/3600
     ax.set_title("{:.3f}Hr".format(sumall),fontsize="10")
 
-    ax2.legend(bbox_to_anchor=(0.5, 1))
+    ax2.legend(bbox_to_anchor=(0, 1),loc = "lower left",fontsize = "large")
     
     st.pyplot(fig)
 
