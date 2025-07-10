ffcf87de3faab67d5f2fe5ca1e884ce154ef893e
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-10 11:43:50 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -76,104 +76,108 @@ if sample_f is not None:
         x_div_num = st.number_input('x軸分割数', value = 20)
         y_div_num = st.number_input('y軸分割数', value = 20)
 
+if st.button("計算を実行"):
+    # ボタンが押されたときに計算を実行
 #データフレームを読み込み累積時間を計算する
-if dataframes:
-    total_counts = {}#この初期化した辞書型へ読み込んで全ロードデータを保存しておく
+    if dataframes:
+        total_counts = {}#この初期化した辞書型へ読み込んで全ロードデータを保存しておく
+        
+        #データ積算とグラフを作成する
+        st.write("累積データ:")
+        fig = plt.figure(figsize=(10, 6)) 
+        gs = GridSpec(10, 10, figure=fig) 
+        # 上段を横一列に使用
+        ax = fig.add_subplot(gs[ :9 , :9 ], projection='3d' )
+        ax2 = fig.add_subplot(gs[ 7: , 7:])
+        z_sum = {}#チェックボックスにチェックが入っている場合の)#チェックボックスにチェックが入っている場合のみプロットする
+        for filename, df in dataframes.items():
+            with st.sidebar:
+                show_data = st.checkbox("{} を表示".format(filename), value=True)        
+            # 合計結果を表示
+            if show_data:# DataFrameが空でないことを確認
+                if df.empty:
+                    st.warning(f"{filename} は空のファイルです。")
+                    continue    
+                if "Time" in df.columns and sample_columns == 5:
+                    df = df.iloc[1:]#dpuの場合は単位行があるために除外する 
+                    time_format = "%H:%M:%S.%f"
+                    df["Time"]= [datetime.strptime(time_str, time_format) for time_str in df["Time"]]
+                    init_time = df["Time"].iloc[0]
+                    df["Time"] = [(time - init_time).seconds for time in df["Time"]]
+                    df = df.apply(pd.to_numeric, errors='coerce')
+                else:#windarabはカラム名調整
+                    new_columns=[]
+                    for rep in df.columns:
+                        rep = rep[:rep.find("[")]
+                        rep = rep.replace(" ","")
+                        new_columns.append(rep)
+                    df.columns = new_columns
+                    #df = df[sample_par]#同じカラム名にする必要あり
+                ax2.scatter(df[x_pal],df[y_pal],s = 3,label = filename)
     
-    #データ積算とグラフを作成する
-    st.write("累積データ:")
-    fig = plt.figure(figsize=(10, 6)) 
-    gs = GridSpec(10, 10, figure=fig) 
-    # 上段を横一列に使用
-    ax = fig.add_subplot(gs[ :9 , :9 ], projection='3d' )
-    ax2 = fig.add_subplot(gs[ 7: , 7:])
-    z_sum = {}#チェックボックスにチェックが入っている場合の)#チェックボックスにチェックが入っている場合のみプロットする
-    for filename, df in dataframes.items():
-        with st.sidebar:
-            show_data = st.checkbox("{} を表示".format(filename), value=True)        
-        # 合計結果を表示
-        if show_data:# DataFrameが空でないことを確認
-            if df.empty:
-                st.warning(f"{filename} は空のファイルです。")
-                continue    
-            if "Time" in df.columns and sample_columns == 5:
-                df = df.iloc[1:]#dpuの場合は単位行があるために除外する 
-                time_format = "%H:%M:%S.%f"
-                df["Time"]= [datetime.strptime(time_str, time_format) for time_str in df["Time"]]
-                init_time = df["Time"].iloc[0]
-                df["Time"] = [(time - init_time).seconds for time in df["Time"]]
-                df = df.apply(pd.to_numeric, errors='coerce')
-            else:#windarabはカラム名調整
-                new_columns=[]
-                for rep in df.columns:
-                    rep = rep[:rep.find("[")]
-                    rep = rep.replace(" ","")
-                    new_columns.append(rep)
-                df.columns = new_columns
-                #df = df[sample_par]#同じカラム名にする必要あり
-            ax2.scatter(df[x_pal],df[y_pal],s = 3,label = filename)
- 
-    #分割数　10として　3Dマップを作る 10分割が１以下になる場合の処理追加必要
-            
-            x_span = (x_upper_bound - x_lower_bound)/x_div_num
-            y_span = (y_upper_bound - y_lower_bound)/y_div_num    
-            
-            for xx in range(x_div_num):
-                x = xx * x_span + int(x_lower_bound)
-                z_sum[x] = {}    
-                for yy in range(y_div_num):
-                    y = yy * y_span + int(y_lower_bound)
-                    # NumPyを使用してフィルタリング
-                    mask_x = (df[x_pal] >= x) & (df[x_pal] < x + x_span)
-                    mask_y = (df[y_pal] >= y) & (df[y_pal] < y + y_span)
-                    filtered_data = df[mask_x & mask_y]
-                    z_sum[x][y] = len(filtered_data)
+        #分割数　10として　3Dマップを作る 10分割が１以下になる場合の処理追加必要
+                
+                x_span = (x_upper_bound - x_lower_bound)/x_div_num
+                y_span = (y_upper_bound - y_lower_bound)/y_div_num    
+                
+                for xx in range(x_div_num):
+                    x = xx * x_span + int(x_lower_bound)
+                    z_sum[x] = {}    
+                    for yy in range(y_div_num):
+                        y = yy * y_span + int(y_lower_bound)
+                        # NumPyを使用してフィルタリング
+                        mask_x = (df[x_pal] >= x) & (df[x_pal] < x + x_span)
+                        mask_y = (df[y_pal] >= y) & (df[y_pal] < y + y_span)
+                        filtered_data = df[mask_x & mask_y]
+                        z_sum[x][y] = len(filtered_data)
 
-            # z_sumを total_counts に追加
-            for x in z_sum:
-                if x not in total_counts:
-                    total_counts[x] = {}
-                for y in z_sum[x]:
-                    total_counts[x][y] = total_counts.get(x, {}).get(y, 0) + z_sum[x][y]
+                # z_sumを total_counts に追加
+                for x in z_sum:
+                    if x not in total_counts:
+                        total_counts[x] = {}
+                    for y in z_sum[x]:
+                        total_counts[x][y] = total_counts.get(x, {}).get(y, 0) + z_sum[x][y]
 
-    x_values = []
-    y_values = []
-    z_values = []
+        x_values = []
+        y_values = []
+        z_values = []
 
-    for x in total_counts:
-        for y in total_counts[x]:
-            x_values.append(x)
-            y_values.append(y)
-            z_values.append(total_counts[x][y])
+        for x in total_counts:
+            for y in total_counts[x]:
+                x_values.append(x)
+                y_values.append(y)
+                z_values.append(total_counts[x][y])
 
-    ax.bar3d(x_values, y_values, 0 , dx=x_span/4 , dy=y_span/4 , dz=z_values , shade=True)
+        ax.bar3d(x_values, y_values, 0 , dx=x_span/4 , dy=y_span/4 , dz=z_values , shade=True)
 
-    ax.set_xlabel(x_pal)
-    ax.set_ylabel(y_pal)
-    ax.set_zlabel("time(sec)")
-    sumall = sum(z_values)/3600
-    ax.set_title("{:.3f}Hr".format(sumall),fontsize="10")
+        ax.set_xlabel(x_pal)
+        ax.set_ylabel(y_pal)
+        ax.set_zlabel("time(sec)")
+        sumall = sum(z_values)/3600
+        ax.set_title("{:.3f}Hr".format(sumall),fontsize="10")
 
-    ax2.legend(bbox_to_anchor=(1, 1),loc = "lower right",fontsize = 8)
-    ax2.set_xlim(x_lower_bound,x_upper_bound)
-    ax2.set_ylim(y_lower_bound,y_upper_bound)
-    ax2.set_xlabel(x_pal)
-    ax2.set_ylabel(y_pal)
+        ax2.legend(bbox_to_anchor=(1, 1),loc = "lower right",fontsize = 8)
+        ax2.set_xlim(x_lower_bound,x_upper_bound)
+        ax2.set_ylim(y_lower_bound,y_upper_bound)
+        ax2.set_xlabel(x_pal)
+        ax2.set_ylabel(y_pal)
 
-    st.pyplot(fig)
+        st.pyplot(fig)
 
-    # ダウンロード用のデータを作成
-    download_data = []
-    for x in total_counts:
-        for y in total_counts[x]:
-            download_data.append([x, y, total_counts[x][y]])
-    # CSV形式でデータをダウンロード
-    csv_data = pd.DataFrame(download_data, columns=[x_pal, y_pal, 'Count'])
-    csv_buffer = csv_data.to_csv(index=False).encode('utf-8')
+        # ダウンロード用のデータを作成
+        download_data = []
+        for x in total_counts:
+            for y in total_counts[x]:
+                download_data.append([x, y, total_counts[x][y]])
+        # CSV形式でデータをダウンロード
+        csv_data = pd.DataFrame(download_data, columns=[x_pal, y_pal, 'Count'])
+        csv_buffer = csv_data.to_csv(index=False).encode('utf-8')
 
-    st.download_button(
-        label="積算データをダウンロード",
-        data=csv_buffer,
-        file_name='cumulative_data.csv',
-        mime='text/csv'
-    )
+        st.download_button(
+            label="積算データをダウンロード",
+            data=csv_buffer,
+            file_name='cumulative_data.csv',
+            mime='text/csv'
+        )
+else:
+    st.write("計算を実行するにはボタンを押してください。")
