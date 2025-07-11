fb1e93428efa2fb4735a25f75ac3585d1032b954
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-11 16:09:32 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -192,10 +192,10 @@ if dataframes:
     fig.update_yaxes(range=[y_lower_bound,y_upper_bound], row=1, col=2)  # Y 軸の上下限
 
 # 左右のグラフの幅を設定（7:3）    
-    fig.update_xaxes(domain=[0, 1], row=1, col=1)  # 3D グラフ（左側）
+    #fig.update_xaxes(domain=[0, 1], row=1, col=1)  # 3D グラフ（左側）
     fig.update_xaxes(domain=[0.7, 1], row=1, col=2)  # 2D グラフ（右側）
 
-    fig.update_yaxes(domain=[0, 1], row=1, col=1)  # 2D グラフ（右側）
+    #fig.update_yaxes(domain=[0, 1], row=1, col=1)  # 2D グラフ（右側）
     fig.update_yaxes(domain=[0.7, 1], row=1, col=2)  # 2D グラフ（右側）
 
     if 'show_graph' not in st.session_state:
