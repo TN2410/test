a0e515498a9fe5c425e74ce85581198ac08edf6a
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-14 15:26:10 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -195,8 +195,8 @@ if dataframes:
 
 # グラフの表示
     if st.session_state.show_graph:
-        st.plotly_chart(fig1 , use_container_width=True)2
-        st.plotly_chart(fig , use_container_width=True)
+        st.plotly_chart(fig1 , use_container_width=True)
+        st.plotly_chart(fig2 , use_container_width=True)
 
     # ダウンロード用のデータを作成
     download_data = []
