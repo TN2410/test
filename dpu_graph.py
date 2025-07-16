35ac6e40dded0f0d9e334646ec104496e2443727
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-16 16:29:06 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -70,9 +70,9 @@ if sample_f is not None:
         initial_y_value = "P_Manifold1"
     else:
         initial_x_value = "nmot"
-        initial_y_value = "kl"
+        initial_y_value = "rl"
 
-    if initial_x_value in sample_par:#and initial_y_value in sample_par:
+    if initial_x_value in sample_par and initial_y_value in sample_par:
         initial_x_index = sample_par.index(initial_x_value)
         initial_y_index = sample_par.index(initial_y_value)
 
