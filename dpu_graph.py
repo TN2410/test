b285d1a87985f5cea7dd8bcab8dde23c6b6f4753
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-18 16:01:14 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -131,6 +131,8 @@ if sample_f is not None:
 
     sample_columns = 5# if skiprow == 2 else 2  # サンプルカラム数の確認
     sample_par = sample_df.iloc[1:,sample_columns].tolist()
+    sample_par = sample_par.dropna()
+
     st.write(sample_par)
 
     with st.sidebar:
