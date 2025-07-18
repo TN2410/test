ebad7b205deb9e06706691e042eeb8e598423ea8
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-18 16:19:04 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -130,7 +130,7 @@ if sample_f is not None:
     sample_df = pd.read_csv(sample_f, encoding='CP932')
 
     # skiprows1を使用してsample_columnsを設定
-    sample_columns = 2# if skiprows == 2 else 5  # サンプルカラム数の確認
+    sample_columns = 2 if skiprows == 2 else 5  # サンプルカラム数の確認
     sample_par = sample_df.iloc[1:, sample_columns].tolist()
     st.write(sample_par)
     sample_par = [x for x in sample_par if pd.notna(x)]  # リスト内包表記を使ってNaNを除外
