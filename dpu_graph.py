2547059098e35083f03fab0a40bf103caf925927
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-21 13:27:40 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -188,3 +188,6 @@ if len(total_counts1) > 0 and len(total_counts2) > 0:
     # 2つの正規化されたzデータの有意差を検出
     t_statistic, p_value = stats.ttest_ind(normalized_z_values1, normalized_z_values2)
     st.write(f"t統計量: {t_statistic}, p値: {p_value}")
+else:
+    if len(total_counts1) > 0:
+        st.write("データフレーム1のグラフが表示されましたが、データフレーム2はアップロードされていません。") 
