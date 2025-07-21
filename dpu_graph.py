ec23d85fc44c125c74298965c16fe891424a6a72
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-21 10:25:36 +0900

    1st commit

--- dpu_graph.py
+++ dpu_graph.py
@@ -135,7 +135,6 @@ if uploaded_files2 is not None:
 if sample_f is not None:
     sample_df = pd.read_csv(sample_f, encoding='CP932')
     # skiprowsを使用してsample_columnsを設定
-    skiprows = 0
     sample_columns = 5 if skiprows == 0 else 2  # サンプルカラム数の確認
     sample_par = sample_df.iloc[1:, sample_columns].tolist()
     sample_par = list(filter(pd.notna, sample_par))
