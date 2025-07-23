373abb39c06f8bb9c83e494564acf50b07bdcebd
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-07-23 09:50:01 +0900

    1st commit

--- dpu_graph_syuusei.py
+++ dpu_graph_syuusei.py
@@ -1,4 +1,4 @@
-# dpuデータをstreamlitで表示する
+# 必要なライブラリのインポート
 import os
 import streamlit as st
 import pandas as pd
@@ -8,6 +8,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from scipy import stats
 
+# Streamlitの設定
 st.set_page_config(
     page_title="PLOT",
     layout="wide",
@@ -21,7 +22,6 @@ def load_uploaded_file(uploaded_file, skiprows):
 
 def process_files(uploaded_files, specific_string):
     dataframes = {}
-    skipropws = 0
     for uploaded_file in uploaded_files:
         initial_lines = pd.read_csv(uploaded_file, nrows=2)
         uploaded_file.seek(0)
@@ -32,8 +32,7 @@ def process_files(uploaded_files, specific_string):
 
 def create_fig(dataframes, x_pal, y_pal, x_lower_bound, x_upper_bound, y_lower_bound, y_upper_bound, x_div_num, y_div_num, skiprows):
     total_counts = {}
-    z_sum = {}
-
+    z_values_dict = {}  # 各ペアのz値を保存する辞書
     all_z_values = []  # すべてのz値を保存するリスト
     x_values = []  # xの値を保存するリスト
     y_values = []  # yの値を保存するリスト
@@ -42,9 +41,9 @@ def create_fig(dataframes, x_pal, y_pal, x_lower_bound, x_upper_bound, y_lower_b
         rows=1,
         cols=2,
         specs=[[{"type": "surface"}, {"type": "scatter"}]],
-        subplot_titles=("時間頻度", "Scatter Plot"),  # タイトルに合計を追加
+        subplot_titles=("時間頻度", "Scatter Plot"),
     )
-    
+
     for filename, df in dataframes.items():
         if df.empty:
             st.warning(f"{filename} は空のファイルです。")
@@ -72,37 +71,38 @@ def create_fig(dataframes, x_pal, y_pal, x_lower_bound, x_upper_bound, y_lower_b
 
         for xx in range(x_div_num):
             x = xx * x_span + int(x_lower_bound)
-            z_sum[x] = {}
-            
             for yy in range(y_div_num):
                 y = yy * y_span + int(y_lower_bound)
                 mask_x = (df[x_pal] > x) & (df[x_pal] <= x + x_span)
                 mask_y = (df[y_pal] > y) & (df[y_pal] <= y + y_span)
                 filtered_data = df[mask_x & mask_y]
-                z_sum[x][y] = len(filtered_data)
-                all_z_values.append(z_sum[x][y])  # z値をリストに追加
-                x_values.append(x)  # xの値を追加
-                y_values.append(y)  # yの値を追加
-        
+                z_value = len(filtered_data)
+                
+                # z値を辞書に保存
+                if z_value > 0:
+                    z_values_dict[(x, y)] = z_value
+                    all_z_values.append(z_value)
+                    x_values.append(x)
+                    y_values.append(y)
+
         # スキャッタープロットの追加
         fig.add_trace(go.Scatter(x=df[x_pal], y=df[y_pal], mode='markers', name=filename), row=1, col=2)
 
-    # z値の合計を計算
-    total_z_value = sum(all_z_values)  # z値の合計を計算
-   
     # タイトルを更新
+    total_z_value = sum(all_z_values)
     fig.layout.annotations[0].text = f"時間頻度 (Total: {total_z_value/3600:.2f}Hrs)"
 
     # z値の正規化
-    normalized_z_values = []  # ここで初期化
+    normalized_z_values = []
     
     if all_z_values:  # z値が存在する場合
         min_z = min(all_z_values)
         max_z = max(all_z_values)
-        if (max_z - min_z) !=0:
+        if (max_z - min_z) != 0:
             normalized_z_values = [(z - min_z) / (max_z - min_z) for z in all_z_values]
-        else :
+        else:
             normalized_z_values = all_z_values
+
         # 正規化したz値を使用して3Dグラフを
         for i in range(len(normalized_z_values)):
             fig.add_trace(go.Scatter3d(
@@ -122,24 +122,24 @@ def create_fig(dataframes, x_pal, y_pal, x_lower_bound, x_upper_bound, y_lower_b
                 line=dict(width=10, color="blue"),
                 showlegend=False
             ), row=1, col=1)
-        
+
     # y軸の上下限を設定
-    fig.update_xaxes(range=[x_lower_bound, x_upper_bound], row=1, col=2)  # Scatter Plotのy軸の上下限を設定
-    fig.update_yaxes(range=[y_lower_bound, y_upper_bound], row=1, col=2)  # Scatter Plotのy軸の上下限を設定
+    fig.update_xaxes(range=[x_lower_bound, x_upper_bound], row=1, col=2)
+    fig.update_yaxes(range=[y_lower_bound, y_upper_bound], row=1, col=2)
 
     fig.update_layout(
         scene=dict(
             camera=dict(
-                eye=dict(x=1, y=-1, z=1)  # 初期の視点を設定
+                eye=dict(x=1, y=-1, z=1)
             ),
-            xaxis_title=x_pal,  # x軸ラベルを設定
-            yaxis_title=y_pal,  # y軸ラベルを設定
-            zaxis_title='頻度'  # z軸ラベルを設定
+            xaxis_title=x_pal,
+            yaxis_title=y_pal,
+            zaxis_title='頻度'
         ),
-        xaxis_title=x_pal,  # Scatter Plotのx軸ラベル
-        yaxis_title=y_pal   # Scatter Plotのy軸ラベル
+        xaxis_title=x_pal,
+        yaxis_title=y_pal
     )
-    return fig, total_counts, normalized_z_values, total_z_value
+    return fig, z_values_dict, normalized_z_values, total_z_value
 
 # ファイルのアップロード
 col1, col2, col3 = st.columns([3, 3, 1])
@@ -153,58 +153,61 @@ with col3:
 specific_string = "windarab"
 
 # ファイルの読み込み
+dataframes, skiprows = None, 0
 if uploaded_files is not None:
     dataframes, skiprows = process_files(uploaded_files, specific_string)
-
 if uploaded_files2 is not None:
     dataframes2, skiprows = process_files(uploaded_files2, specific_string)
 
 # グラフのパラメータ設定
 if sample_f is not None:
     sample_df = pd.read_csv(sample_f, encoding='CP932')
-    # skiprowsを使用してsample_columnsを設定
-    sample_columns = 5 if skiprows == 0 else 2  # サンプルカラム数の確認
+    sample_columns = 5 if skiprows == 0 else 2
     sample_par = sample_df.iloc[1:, sample_columns].tolist()
     sample_par = list(filter(pd.notna, sample_par))
     
-    #初期表示パラメータを設定
-    if skiprows == 0 :
-        initial_x_pal , initial_y_pal = "NE" , "P_Manifold1"
-        initial_x_lower_bound , initial_x_upper_bound = 0 , 8000 
-        initial_y_lower_bound , initial_y_upper_bound = 0 , 250
-    else :
-        initial_x_pal , initial_y_pal = "nmot" , "pboost"
-        initial_x_lower_bound , initial_x_upper_bound = 0 , 8000 
-        initial_y_lower_bound , initial_y_upper_bound = 0 , 2000
+    if skiprows == 0:
+        initial_x_pal, initial_y_pal = "NE", "P_Manifold1"
+        initial_x_lower_bound, initial_x_upper_bound = 0, 8000 
+        initial_y_lower_bound, initial_y_upper_bound = 0, 250
+    else:
+        initial_x_pal, initial_y_pal = "nmot", "pboost"
+        initial_x_lower_bound, initial_x_upper_bound = 0, 8000 
+        initial_y_lower_bound, initial_y_upper_bound = 0, 2000
 
     with st.sidebar:
-        x_pal = st.selectbox('x列を選択してください', sample_par,index=sample_par.index(initial_x_pal))
-        y_pal = st.selectbox('y列を選択してください', sample_par,index=sample_par.index(initial_y_pal))
-        x_lower_bound = st.number_input('xの下限値と', value=initial_x_lower_bound, step=1)
+        x_pal = st.selectbox('x列を選択してください', sample_par, index=sample_par.index(initial_x_pal))
+        y_pal = st.selectbox('y列を選択してください', sample_par, index=sample_par.index(initial_y_pal))
+        x_lower_bound = st.number_input('xの下限値を入力してください', value=initial_x_lower_bound, step=1)
         x_upper_bound = st.number_input('xの上限値を入力してください', value=initial_x_upper_bound, step=10)
-        y_lower_bound = st.number_input('yの下限値と', value=initial_y_lower_bound, step=1)
+        y_lower_bound = st.number_input('yの下限値を入力してください', value=initial_y_lower_bound, step=1)
         y_upper_bound = st.number_input('yの上限値を入力してください', value=initial_y_upper_bound, step=10)
         x_div_num = st.number_input('x軸分割数', value=20)
         y_div_num = st.number_input('y軸分割数', value=20)
 
 # グラフの作成
-
 if dataframes:
-    fig1, total_counts1, normalized_z_values1, total_z_value1 = create_fig(dataframes, x_pal, y_pal, x_lower_bound, x_upper_bound, y_lower_bound, y_upper_bound, x_div_num, y_div_num, skiprows)
+    fig1, z_values1, normalized_z_values1, total_z_value1 = create_fig(dataframes, x_pal, y_pal, x_lower_bound, x_upper_bound, y_lower_bound, y_upper_bound, x_div_num, y_div_num, skiprows)
     st.plotly_chart(fig1)
 
 if dataframes2:
-    fig2, total_counts2, normalized_z_values2, total_z_value2 = create_fig(dataframes2, x_pal, y_pal, x_lower_bound, x_upper_bound, y_lower_bound, y_upper_bound, x_div_num, y_div_num, skiprows)
+    fig2, z_values2, normalized_z_values2, total_z_value2 = create_fig(dataframes2, x_pal, y_pal, x_lower_bound, x_upper_bound, y_lower_bound, y_upper_bound, x_div_num, y_div_num, skiprows)
     st.plotly_chart(fig2)
 
 # 有意差の検出
+if z_values1 and z_values2:
+    # 各z値のリストを作成
+    z_values_1 = list(z_values1.values())
+    z_values_2 = list(z_values2.values())
 
-    t_statistic, p_value = stats.ttest_ind(normalized_z_values1, normalized_z_values2)
-    st.write(f"U統計量: {t_statistic:.3f}, p値: {p_value:.3g}")
-    if p_value < 0.05:
-        st.write("有意差あり")
-    u_statistic, p_value = stats.mannwhitneyu(normalized_z_values1, normalized_z_values2, alternative='two-sided')
-    st.write(f"t統計量: {t_statistic:.3f}, p値: {p_value:.3g}")
-    if p_value < 0.05:
+    # t検定
+    t_statistic, p_value_t = stats.ttest_ind(z_values_1, z_values_2)
+    st.write(f"t統計量: {t_statistic:.3f}, p値: {p_value_t:.3g}")
+    if p_value_t < 0.05:
         st.write("有意差あり")
 
+    # Mann-Whitney U検定
+    u_statistic, p_value_u = stats.mannwhitneyu(z_values_1, z_values_2, alternative='two-sided')
+    st.write(f"U統計量: {u_statistic:.3f}, p値: {p_value_u:.3g}")
+    if p_value_u < 0.05:
+        st.write("有意差あり")
