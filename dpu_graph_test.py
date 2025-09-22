129e2e2179a5f03c8bf507c998f86a5a03c347e6
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-09-22 13:24:09 +0900

    1st commit

--- dpu_graph_test.py
+++ dpu_graph_test.py
@@ -2,22 +2,7 @@ import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
 
-st.title("Plotlyテスト用ヒストグラム")
+st.write("Hello Streamlit and Plotly")
 
-# テスト用のデータ生成
-np.random.seed(0)
-data = np.random.randn(500)
-
-# ヒストグラム作成
-fig = go.Figure(
-    data=[go.Histogram(x=data, nbinsx=30, marker_color='navy', opacity=0.7)]
-)
-
-fig.update_layout(
-    title="ランダム正規分布データのヒストグラム",
-    xaxis_title="値",
-    yaxis_title="度数",
-    template="simple_white"
-)
-
-st.plotly_chart(fig, use_container_width=True)
+fig = go.Figure(go.Bar(y=[2, 1, 3]))
+st.plotly_chart(fig)
