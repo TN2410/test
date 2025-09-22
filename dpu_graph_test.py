e817415aecd58af1549020dea398061724ec4d4e
TN2410 <tsuneyoshi_nishida@mail.toyota.co.jp>
2025-09-22 13:40:33 +0900

    1st commit

--- dpu_graph_test.py
+++ dpu_graph_test.py
@@ -1,8 +1,2 @@
-import streamlit as st
 import plotly.graph_objects as go
-import numpy as np
-
-st.write("Hello Streamlit and Plotly")
-
-fig = go.Figure(go.Bar(y=[2, 1, 3]))
-st.plotly_chart(fig)
+print(go)
